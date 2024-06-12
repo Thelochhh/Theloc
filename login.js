043f821c7f4b29fff8d1a2f5dfbfe4585a874007
Thế Lộc <127642873+Thelochhh@users.noreply.github.com>
2024-06-12 09:26:52 +0700

    Test thay doi tu web->may

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function Kt(){
     const password = document.getElementById('password').value;
 
 if(username == '' && password==''){
-    alert('Vui lòng nhập đầy đủ thông tin thang ngu nay!');
+    alert('Vui lòng nhập đầy đủ thông tin DCMM!');
 }
 else if(username != 'Theloc' && password != 'Theloc1610'){
         alert('Sai thông tin đăng nhập. Vui lòng thử lại!');
